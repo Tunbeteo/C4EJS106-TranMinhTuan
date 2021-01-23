21acc948a809ec60804a770ce1eff8dee3e1fce0
tunbeteo <tunbeteo@gmail.com>
2021-01-23 22:10:51 +0700

    update lesson2

---
+++ Lesson2/Lesson2.js
@@ -0,0 +1,88 @@
+
+//let tenmon1 =prompt("Nhập Tên Món ăn");
+//let giamon1 =prompt("Giá món tương ứng");
+//let tenmon2 =prompt("Nhập Tên Món ăn");
+//let giamon2 =prompt("Giá món tương ứng");
+//let tenmon3 =prompt("Nhập Tên Món ăn");
+//let giamon3 =prompt("Giá món tương ứng");
+//console.log(`${tenmon1}${giamon1}`);
+//console.log(`${tenmon2}${giamon2}`);
+//console.log(`${tenmon3}${giamon3}`);
+
+//for (let i=0;i<3;i++){
+  //  let tenmon =prompt("enter name of item");
+   // let giamon =prompt("enter price of item");
+  //  console.log(`${tenmon}${giamon}`);
+//}
+//console.log("bai1")
+    //for (let i=0;i<=10;i++){
+        //console.log(i);
+    //}
+    //console.log("bai2")
+   // for (let i=10;i>=0;i--){
+        //console.log(i);
+   // }
+   // console.log("bai3")
+   // for (let i=0;i<=50;i=i+2){
+      //  console.log(i);
+    
+  //  }
+
+  //let m =prompt("nhập m");
+  //let n =prompt('nhập n');
+  //for (let i=m;i<=n;i++){
+     // console.log(i);
+  //}
+
+//   let m =prompt("nhập m");
+//   let n =prompt('nhập n');
+//   while (m<=n){
+//       console.log(m);
+//       m++;
+//   }
+
+//while chưa xác định ( bước nhảy)
+//for xác định ( bước nhảy)
+
+//in ra các số chia hết cho a
+// let n=prompt('nhập n');
+// for (let i=3;i<=n;i=i+3){
+// console.log(i);
+// 
+
+// let n=Number( prompt('nhập n'));
+// let m=Number (prompt('nhập m'));
+// for (let i=m;i<=n;i++){
+//     if (i%3 == 0)
+//      console.log("chia hết cho 3");
+//      else if(i%5 == 0){
+//          console.log("chia het cho 5")
+//          else {
+//              console.log("failed")
+//          }
+//      }
+// }
+
+// yêu cầu người dùng nhập tuổi in ra xem người dùng đã đủ tuổi lấy vợ chưa
+// nếu tuổi nhỏ hơn 10 =>) trẻ con 
+// Nếu tuổi <20 => chưa đủ tuổi
+//nếu tuổi <60 => oke 
+//> 60 già quá
+ 
+
+// kiểm tra xem cái dòng nào bị đánh dấu dưới ~ thì là sai
+
+let tuoi=Number (prompt('Nhập tuổi xem được cưới vợ không ?'));
+if(tuoi<=10){
+    console.log("trẻ con")
+}
+    else if(tuoi<=20){
+        console.log('chưa đủ tuổi')
+    }
+        else if(tuoi<=60){
+            console.log("oke")
+        }
+            else {
+                console.log("già quá")
+            }
+            
