1339a04789ac1e6918d2aff5fcad369d5bd71df9
tunbeteo <tunbeteo@gmail.com>
2021-03-16 21:04:16 +0700

    hi

---
+++ Buổi 6/Study.js
@@ -0,0 +1,19 @@
+// baif1 
+// // console.log(Math.floor(Math.random() * 1))
+
+// //bài 2
+// let num = [1,3,4,5,7,8,9,10];
+// let hinum = num[Math.floor(Math.random()*num.length)];
+// console.log(hinum)
+
+
+// bài 3
+// Ứng dụng kiểm soát kho
+// Cho phép người dùng quản trị được số tình hình bán hàng , lượng sản phẩm tiêu thụ
+// Hoạt động của ứng dụng.
+// Phân quyền quản trị màn hình đăng nhập
+// Nhập dữ liệu thực tế đầu , nhập 
+//  nhập dư liệu từ POS // số liệu sử dụng
+// tính toán ra số liệu tồn // xuất báo cáo xu hướng//
+
+
