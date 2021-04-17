c9aef002a11d20e7a2229d94884f3a93e1c1f482
tunbeteo <tunbeteo@gmail.com>
2021-04-17 22:07:22 +0700

    bai test

---
+++ hack/Main.js
@@ -0,0 +1,9 @@
+// var n=prompt("nhập")
+// function findOpposite(n){
+//     return -n
+// }
+// console.log(n);
+function myFunction() {
+document.getElementsByid("nhap").submit()
+console.log(so);
+}
