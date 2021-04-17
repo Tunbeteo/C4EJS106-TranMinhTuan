9a98e86d5393be17c7d035bdee4986b2bb716540
tunbeteo <tunbeteo@gmail.com>
2021-04-17 22:25:27 +0700

    test

--- hack/Main.js
+++ hack/Main.js
@@ -10,12 +10,12 @@ let kq = TaoSoNgauNhien(a,b);
 
 content.innerText+=`Dự đoán con số của bạn nằm trong khoảng ${a} đến ${b}`;
 let valueInput=document.querySelector("#input_");
-let Quay__=document.querySelector("#Quay__");
+let bamquay=document.querySelector("#bamquay");
 let count=3;
 
 
 
-Quay__.onclick=()=>{
+bamquay.onclick=()=>{
     
     console.log(valueInput.value);
     if(valueInput.value != kq){
@@ -23,7 +23,7 @@ Quay__.onclick=()=>{
         content.innerText+=` -- Bạn dự đoán sai còn ${count} lần nữa`;
     }
     else{
-        alert("Bạn đúng kết quả là kq");
+        alert("Bạn đúng kết quả là kết quả");
     }
     if(count==0){
         alert("Ban đã hết lượt dự đoán")
