7d2f2b158fd3b11dc6908f1910f4bb4af1c3c570
tunbeteo <tunbeteo@gmail.com>
2021-04-18 03:08:44 +0700

    test

--- hack/Main.js
+++ hack/Main.js
@@ -1,34 +1,20 @@
-
-
-let a=1,b=10;
-let content=document.querySelector(".content");
-function TaoSoNgauNhien(min, max){
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-let kq = TaoSoNgauNhien(a,b);
-
-content.innerText+=`Dự đoán con số của bạn nằm trong khoảng ${a} đến ${b}`;
-let valueInput=document.querySelector("#input_");
-let bamquay=document.querySelector("#bamquay");
-let count=3;
-
-
-
-bamquay.onclick=()=>{
-    
-    console.log(valueInput.value);
-    if(valueInput.value != kq){
-        count--;
-        content.innerText+=` -- Bạn dự đoán sai còn ${count} lần nữa`;
+let ketqua = Math.floor(Math.random() * 11);
+// console.log(valueInput);
+let btn = document.getElementById("btn");
+btn.addEventListener('click', function () {
+    let input = document.getElementById("input_")
+    let valueInput = input.value
+    console.log(valueInput);
+    let count = 3;
+    if (valueInput == ketqua) {
+        alert("Bạn đã Trúng Thưởng")
+    } else {
+        count--
+        alert(`Bạn đã đoán sai còn${count}lần nữa`)
+    } if (count == 0) {
+        alert("Bạn đã hết lượt")
     }
-    else{
-        alert("Bạn đúng kết quả là kết quả");
-    }
-    if(count==0){
-        alert("Ban đã hết lượt dự đoán")
-        
-    }
-}
+    input.value=''
+})
 
-console.log(kq);
+console.log(ketqua);
